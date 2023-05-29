0e9971f14dbcbe6c6bf94191a97aae326b9c2420
Angel Gaitan <133923765+AngelGaitan@users.noreply.github.com>
2023-05-29 15:46:45 -0600

    card for the cart

--- assets/utils/carrito.js
+++ assets/utils/carrito.js
@@ -1,16 +1,15 @@
 
 let carrito = []
 
-let carStatus = document.querySelector(".car_status")
+let carStatus = document.querySelector(".car_status");
 
 function addToCart(id) {
+    let product = 0
     for (const item of data1) {
-        let product = 0
         if (item.id === id) {
-          product+= 
-         carrito.push(product)
+         product+= 
+         carrito.push(product) 
         }
     }
-    carStatus.innerHTML = carrito.length
+carStatus.innerHTML = carrito.length
 }
-
